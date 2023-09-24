4381a3d51dc3e5e4ff7752835dbfb8e9f18fb514
Its_Shojib <mdshojib922@gmail.com>
2023-09-24 22:02:38 +0600

    Json File and Create Banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,20 @@
 
 const Banner = () => {
     return (
-        <div className="flex justify-center items-center">
-            <div>
-                <h1  className="">I Grow By Helping People In Need</h1>
+        <div>
+            <div className="hero min-h-[400px]" style={{ backgroundImage: 'url("/Rectangle 4287.png")' }}>
+                <div className="hero-overlay bg-white bg-opacity-95"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="flex justify-center items-center">
+                        <div className="space-y-5">
+                            <h1 className="text-black font-bold text-3xl">I Grow By Helping People In Need</h1>
+                            <input className="w-64 p-2 border-solid border-2" type="text" name="" id="" placeholder="Search here..."/>
+                            <button className="bg-red-500 text-white px-3 py-2 rounded-r-md">Search</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+
 
         </div>
     );
--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header/Header";
-import Banner from './../Components/Banner/Banner';
+
 
 
 const MainLayout = () => {
@@ -8,7 +8,6 @@ const MainLayout = () => {
         <div>
             <div className="max-w-screen-xl mx-auto px-4">
                 <Header></Header>
-                <Banner></Banner>
             </div>
             <div className="max-w-screen-xl mx-auto px-5">
                 <Outlet></Outlet>
